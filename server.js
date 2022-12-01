18d44bb0fbb79438d01bddf06e9ba537da7bf888
Mike Alex <devmikealex@gmail.com>
2022-12-01 21:52:51 +0300

    Change server port

--- server.js
+++ server.js
@@ -1,35 +1,38 @@
-import clc from "cli-color"
+import clc from 'cli-color'
 import clipboardy from 'clipboardy'
-import express from "express"
-import bodyParser from "body-parser"
-import fs from "fs"
+import express from 'express'
+import bodyParser from 'body-parser'
+import fs from 'fs'
 // const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-import fetch from "node-fetch"
+import fetch from 'node-fetch'
 
-
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3030
 
 const app = express()
 // app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 // app.use(bodyParser.text());
 
-app.use((req,res,next) => {
-    console.log(clc.magentaBright('---middleware:'), clc.green(req.method), req.originalUrl)
+app.use((req, res, next) => {
+    console.log(
+        clc.magentaBright('---middleware:'),
+        clc.green(req.method),
+        req.originalUrl
+    )
     next()
 })
 
 app.use(express.static('.'))
 
-app.get("/", (req, res) => {
-    res.redirect("getgame.html")
+app.get('/', (req, res) => {
+    res.redirect('getgame.html')
 })
-app.post("/", (req, res) => {
+app.post('/', (req, res) => {
     console.log('POST', req.body)
     res.status(200).send('ooOKkk')
 })
 
-app.get("/clipboard", (req, res) => {
+app.get('/clipboard', (req, res) => {
     const clip = clipboardy.readSync().trim()
     console.log(clc.greenBright('Clipboard Read:'), clip)
     if (clip.includes('store.steampowered.com/app')) {
@@ -49,19 +52,19 @@ app.get("/clipboard", (req, res) => {
     }
 })
 
-app.get("/game/:id", (req, res) => {
+app.get('/game/:id', (req, res) => {
     console.log(clc.cyanBright('GAME:'), req.params)
     console.log(clc.cyanBright('QUERY:'), req.query)
     const gameID = req.params.id
-    const cursor = req.query.cursor || '*';
+    const cursor = req.query.cursor || '*'
 
-    if (gameID==='0') {
-        console.log('ZERO');
+    if (gameID === '0') {
+        console.log('ZERO')
         res.send('ZERO Game')
         return
     }
-    
-    (async function main() {
+
+    ;(async function main() {
         const urlGameInfo = `https://store.steampowered.com/api/appdetails?appids=${gameID}`
         const responseGI = await fetch(urlGameInfo)
         const gameInfo = await responseGI.json()
@@ -70,17 +73,17 @@ app.get("/game/:id", (req, res) => {
             console.log(clc.redBright('Game not found'))
             res.send('Game not found')
             return
-        } 
+        }
         // console.log(Object.hasOwn(gameInfo, gameID))
         // console.log(Object.hasOwn(gameInfo, 'data'))
         // console.log(Object.hasOwn(gameInfo, 'name'))
         const gameName = gameInfo[gameID].data.name
-        console.log(clc.cyanBright('Name:'), gameName);
+        console.log(clc.cyanBright('Name:'), gameName)
 
         const playtimemin = req.query.playtimemin || 10
 
         const url = `https://store.steampowered.com/appreviews/${gameID}?cursor=${cursor}&day_range=30&start_date=-1&end_date=-1&date_range_type=all&filter=recent&language=russian&l=russian&review_type=negative&purchase_type=all&playtime_filter_min=${playtimemin}&playtime_filter_max=0&filter_offtopic_activity=1`
-        console.log(clc.cyanBright("URLrev: ") + clc.yellowBright(url))
+        console.log(clc.cyanBright('URLrev: ') + clc.yellowBright(url))
         const response = await fetch(url)
         const data = await response.json()
         // console.log('Data:', data.html.slice(0,50))
@@ -93,7 +96,7 @@ app.get("/game/:id", (req, res) => {
         newContent = newContent.replace('XXXXXX', data.html)
 
         res.send(newContent)
-        console.log();
+        console.log()
     })()
 })
 
@@ -102,9 +105,11 @@ app.get("/game/:id", (req, res) => {
 // })
 
 app.listen(PORT, () => {
-    console.log();
+    console.log()
     console.log('Start server')
-    console.log('Working Dir:', process.cwd());
+    console.log('Working Dir:', process.cwd())
     console.log(clc.yellowBright(`http://127.0.0.1:${PORT}/getgame.html`))
     console.log(clc.yellowBright(`http://127.0.0.1:${PORT}/clipboard`))
-});
+}).on('error', function (e) {
+    console.log(clc.redBright('--- ! ERROR ! -'), e.message)
+})
